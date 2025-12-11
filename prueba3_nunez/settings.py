f09f6431bff6ac4e1788121434995900efd779fa
Alessandro Núñez <alessandrodani22@gmail.com>
2025-12-11 01:15:55 -0300

    Cambio a SQLite para deployment

--- prueba3_nunez/settings.py
+++ prueba3_nunez/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'prueba3_nunez.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'BD_ALUMNOS',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
@@ -122,6 +118,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STACTICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
